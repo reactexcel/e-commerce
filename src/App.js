19f3fa8d0fd179eec58981b4d4a6b729e64e8b71
reactexcel <arun.etech2011@gmail.com>
2024-09-16 18:06:54 +0530

    added cart

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import Details from "./components/Details";
+import Cart from "./components/Cart";
 import Default from "./components/Default";
 import Modal from './components/Modal';
 import { ThemeConsumer } from './components/context/ThemeContexts'
@@ -19,6 +20,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={ProductList} />
         <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
         <Route component={Default} />
       </Switch>
       <Modal />
